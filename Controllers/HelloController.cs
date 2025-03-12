fc747502574e830b32ed4937e2543ea30015af55
Philip Wade <jphilipwade@gmail.com>
2025-03-12 07:16:26 +0000

    first commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WelshAppApi.Controllers
+{
+    [ApiController]
+    [Route("api/hello")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetHelloMessage()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
